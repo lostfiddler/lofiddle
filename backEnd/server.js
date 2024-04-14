d8fa96e4982c1d215bd4331df8584fd07b08d693
Ian Parkinson <h3lloiain@gmail.com>
2024-04-14 18:46:20 -0500

    doing front-end stuff, should really use the frontEnd branch for this stuff

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,23 +1,40 @@
 import { readFileSync } from 'node:fs';
 import http from 'node:http'
 
-import data from './bookData.js' 
+import {data} from './bookData.js'
 
 const server = http.createServer();
 
 server.on('request', (request, res) => {
+    if(request.url === '/') {
+        res.writeHead(200, {
+            'access-control-allow-origin': '*'
+        })
+        res.write(readFileSync('./frontEnd/index.html'));
+        res.end();
+        return;
+    }
+
+    if(['.html', '.css', '.js'].some(ext => request.url.includes(ext))) {
+        res.writeHead(200, {
+            'access-control-allow-origin': '*',
+            'content-type': {
+                '.js': 'text/javascript',
+                '.css': 'text/css',
+                '.html': 'text/html'
+            }[request.url.slice(request.url.lastIndexOf('.'))]
+        })
+        res.write(readFileSync('./frontEnd/books/' + request.url))
+        res.end();
+        return;
+    }
+
     if(request.url === '/get-books') {
         res.writeHead(200, {'access-control-allow-origin': '*'});
         res.write(JSON.stringify(data));
         res.end();
         return;
     }
-
-    res.writeHead(200, {
-        'access-control-allow-origin': '*'
-    })
-    res.write(readFileSync('./index.html'));
-    res.end();
 });
 
 server.listen(7000, () => {
