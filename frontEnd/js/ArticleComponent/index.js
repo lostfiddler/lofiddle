c92159ae33294e28ce62054a823292a3c4610782
Ian Parkinson <h3lloiain@gmail.com>
2024-10-01 17:51:02 -0500

    fixed prismjs finally

--- frontEnd/js/ArticleComponent/index.js
+++ frontEnd/js/ArticleComponent/index.js
@@ -1,20 +1,31 @@
-import GetArticle from './Article.js'
-import Style from './style.css?inline'
+import GetArticle from './Article.js';
+import prismCSSRaw from '../../node_modules/prismjs/themes/prism-tomorrow.min.css?raw'
+import Style from './style.css?inline';
 
 export default class PostComponent extends HTMLElement {
     constructor() {
         super()
-        this.shadow = this.attachShadow({mode: 'open'})
-        this.styleSheet = document.createElement('style')
+        this.shadow = this.attachShadow({mode: 'open'});
+        this.styleSheet = document.createElement('style');
+        this.prismCSS = document.createElement('style');
 
         window.addEventListener('popstate', async (e) => {
             const path = e.state;
             const module = await GetArticle(path)
 
+            this.articleName = module.title()
             this.canvasApp = module.canvasApp()
             this.article = module.article()
-            this.shadow.replaceChildren(this.styleSheet, this.canvasApp, this.article)
+
+            this.shadow.replaceChildren(
+                this.styleSheet,
+                this.prismCSS,
+                this.articleName,
+                this.canvasApp,
+                this.article
+            )
         });
+
     }
 
     connectedCallback() {
@@ -22,12 +33,9 @@ export default class PostComponent extends HTMLElement {
     }
 
     RenderView() {
+        this.prismCSS.textContent = prismCSSRaw
+        this.shadow.appendChild(this.prismCSS)
         this.styleSheet.textContent = Style;
-
-        const kitten = document.createElement('span');
-        kitten.textContent = 'all you need is love! no, hatred is the only way!';
-
-        this.shadow.append(this.styleSheet, kitten)
     }
 }
 
