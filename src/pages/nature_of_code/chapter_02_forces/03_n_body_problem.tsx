a75d8a4e9ea3e25fb8be085a49988e8ea5cb0a17
lostfiddler <h3lloiain@gmail.com>
2025-08-15 14:00:42 -0500

    added empty page

---
+++ src/pages/nature_of_code/chapter_02_forces/03_n_body_problem.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default () => {
+    return (
+        <div>
+            <h1>N-Body Problem</h1>
+            <p>
+                More info {" "}
+                <a href="https://natureofcode.com/forces/#the-n-body-problem">
+                    here
+                </a>.
+            </p>
+        </div>
+    );
+};
