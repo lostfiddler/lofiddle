a57ca844ea3c03b52b417cd5f21a782cb8576f28
lostfiddler <h3lloiain@gmail.com>
2025-08-15 13:57:35 -0500

    modify page

--- src/pages/three/utiliites.tsx
+++ src/pages/three/utiliites.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import * as THREE from "three";
-import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader';
-import { OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 export default () => {
     const canvasRef = useRef(null);
@@ -11,7 +11,13 @@ export default () => {
     });
     return (
         <div>
-            util functions
+            <h1>Utilities and notes for Beginners</h1>
+            <ul>
+                <li>Size, measurements and units</li>
+                <li>camera and orientation</li>
+                <li>debugging</li>
+                <li>use the console!</li>
+            </ul>
             <canvas ref={canvasRef} width="500" height="700"></canvas>
         </div>
     );
@@ -19,26 +25,26 @@ export default () => {
 
 function canvasApp(c: HTMLCanvasElement) {
     const canvas = c;
-    const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
+    const renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
 
     const scene = new THREE.Scene();
 
     const dirLight = new THREE.DirectionalLight(0xffffff, 1);
-    dirLight.position.set(0, 1, 4)
-    scene.add(dirLight)
+    dirLight.position.set(0, 1, 4);
+    scene.add(dirLight);
 
     const camera = new THREE.PerspectiveCamera();
     camera.position.set(1, 1, 3);
 
     const axes = new THREE.AxesHelper(6);
     const grid = new THREE.GridHelper();
-    scene.add(axes, grid)
+    scene.add(axes, grid);
 
     const gltfLoader = new GLTFLoader();
     const url = "/models/pirate_ship/ship-pirate-large.glb";
     gltfLoader.load(url, (gltf) => {
         const root = gltf.scene;
-        root.scale.set(0.1,0.1,0.1)
+        root.scale.set(0.1, 0.1, 0.1);
         scene.add(root);
     });
 
@@ -46,8 +52,8 @@ function canvasApp(c: HTMLCanvasElement) {
     controls.update();
 
     (function animate() {
-        requestAnimationFrame(animate)
+        requestAnimationFrame(animate);
 
-        renderer.render(scene, camera)
-    })()
+        renderer.render(scene, camera);
+    })();
 }
